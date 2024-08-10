9944f5ae2852816e44635ffa8b9005529c32127c
khanblair <manb10291@gmail.com>
2024-08-10 19:52:14 +0300

    Next auth with clerk & mongodb

---
+++ app/portfolio/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+    </div>
+  )
+}
+
+export default Portfolio;
+
+export const generateMetadata = () => {
+  return {
+    title: "Portfolio page", 
+    description: "This is a portfolio page",
+  }
+}
